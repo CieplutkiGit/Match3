7da63ed21ebabd0b6b5fe1108c2c8a6b071a698d
Kapec <68950131+CieplutkiGit@users.noreply.github.com>
2026-06-08 14:25:58 +0200

    Add DOTween swap, destroy and fall animations

--- Assets/Scripts/View/BoardView.cs
+++ Assets/Scripts/View/BoardView.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using Match3.Model;
 using Match3.Core;
@@ -11,15 +14,19 @@ namespace Match3.View
         [SerializeField] private PieceView _piecePrefab;
         [SerializeField] private PieceSpriteConfig _spriteConfig;
         [SerializeField] private float _spacing = 1.0f;
+        [SerializeField] private float _swapDuration = 0.25f;
+        [SerializeField] private float _fallDuration = 0.3f;
+        [SerializeField] private float _destroyDuration = 0.2f;
 
         private BoardSystem _boardSystem;
         private PieceView[,] _pieceViews;
+
+        public bool IsAnimating { get; private set; }
 
         public void Initialize(BoardSystem boardSystem)
         {
             _boardSystem = boardSystem;
             _pieceViews = new PieceView[_boardSystem.Grid.Width, _boardSystem.Grid.Height];
-            
             SpawnInitialVisuals();
         }
 
@@ -33,19 +40,173 @@ namespace Match3.View
                     var data = grid.Get(x, y);
                     if (data != null && data.Color != PieceColor.None)
                     {
-                        SpawnPieceView(x, y, data.Color, data.Type);
+                        SpawnPieceView(x, y, data.Color, data.Type, false);
                     }
                 }
             }
         }
 
-        private void SpawnPieceView(int x, int y, PieceColor color, PieceType type)
+        private PieceView SpawnPieceView(int x, int y, PieceColor color, PieceType type, bool animate = true)
         {
             var worldPos = GetWorldPosition(x, y);
             var pieceInstance = Instantiate(_piecePrefab, worldPos, Quaternion.identity, transform);
             var sprite = _spriteConfig.GetSprite(color, type);
             pieceInstance.Setup(x, y, color, type, sprite);
             _pieceViews[x, y] = pieceInstance;
+
+            if (animate)
+            {
+                pieceInstance.PlaySpawnAnimation();
+            }
+
+            return pieceInstance;
+        }
+
+        public IEnumerator AnimateSwap(int x1, int y1, int x2, int y2, bool revert)
+        {
+            IsAnimating = true;
+
+            var v1 = GetPieceView(x1, y1);
+            var v2 = GetPieceView(x2, y2);
+
+            if (v1 == null || v2 == null)
+            {
+                IsAnimating = false;
+                yield break;
+            }
+
+            var pos1 = GetWorldPosition(x1, y1);
+            var pos2 = GetWorldPosition(x2, y2);
+
+            bool done = false;
+            int tweensComplete = 0;
+
+            v1.MoveTo(pos2, _swapDuration, Ease.OutCubic, () =>
+            {
+                tweensComplete++;
+                if (tweensComplete >= 2) done = true;
+            });
+            v2.MoveTo(pos1, _swapDuration, Ease.OutCubic, () =>
+            {
+                tweensComplete++;
+                if (tweensComplete >= 2) done = true;
+            });
+
+            yield return new WaitUntil(() => done);
+
+            if (!revert)
+            {
+                _pieceViews[x1, y1] = v2;
+                _pieceViews[x2, y2] = v1;
+                v1.UpdatePosition(x2, y2);
+                v2.UpdatePosition(x1, y1);
+            }
+
+            IsAnimating = false;
+        }
+
+        public IEnumerator AnimateDestroy(List<MatchResult> matches)
+        {
+            IsAnimating = true;
+
+            var piecesToDestroy = new HashSet<PieceView>();
+            foreach (var match in matches)
+            {
+                foreach (var pieceData in match.MatchedPieces)
+                {
+                    var view = GetPieceView(pieceData.X, pieceData.Y);
+                    if (view != null)
+                    {
+                        piecesToDestroy.Add(view);
+                    }
+                }
+            }
+
+            bool done = false;
+            int animatingCount = piecesToDestroy.Count;
+
+            if (animatingCount == 0)
+            {
+                IsAnimating = false;
+                yield break;
+            }
+
+            foreach (var view in piecesToDestroy)
+            {
+                _pieceViews[view.X, view.Y] = null;
+                view.PlayDestroyAnimation(_destroyDuration, () =>
+                {
+                    animatingCount--;
+                    if (animatingCount <= 0) done = true;
+                });
+            }
+
+            yield return new WaitUntil(() => done);
+            IsAnimating = false;
+        }
+
+        public IEnumerator AnimateFall(List<PieceFallInfo> fallInfos, List<PieceData> spawnedPieces)
+        {
+            IsAnimating = true;
+
+            var spawnedPieceSet = new HashSet<PieceData>(spawnedPieces);
+            int tweensComplete = 0;
+            int totalTweens = fallInfos.Count;
+
+            if (totalTweens == 0)
+            {
+                IsAnimating = false;
+                yield break;
+            }
+
+            foreach (var info in fallInfos)
+            {
+                PieceView view;
+                var data = _boardSystem.Grid.Get(info.ToX, info.ToY);
+
+                if (data != null && spawnedPieceSet.Contains(data))
+                {
+                    Vector3 spawnWorldPos = GetWorldPosition(info.FromX, info.FromY);
+                    view = SpawnPieceView(info.ToX, info.ToY, data.Color, data.Type, false);
+                    view.transform.position = spawnWorldPos;
+                }
+                else
+                {
+                    view = _pieceViews[info.ToX, info.ToY];
+                    _pieceViews[info.ToX, info.ToY] = view;
+                }
+
+                if (view == null)
+                {
+                    tweensComplete++;
+                    continue;
+                }
+
+                Vector3 targetWorldPos = GetWorldPosition(info.ToX, info.ToY);
+                view.MoveTo(targetWorldPos, _fallDuration, Ease.OutBounce, () =>
+                {
+                    tweensComplete++;
+                });
+            }
+
+            yield return new WaitUntil(() => tweensComplete >= totalTweens);
+            IsAnimating = false;
+        }
+
+        public void RefreshView()
+        {
+            var grid = _boardSystem.Grid;
+            for (int x = 0; x < grid.Width; x++)
+            {
+                for (int y = 0; y < grid.Height; y++)
+                {
+                    var data = grid.Get(x, y);
+                    if (data != null && _pieceViews[x, y] != null)
+                    {
+                        _pieceViews[x, y].UpdatePosition(x, y);
+                    }
+                }
+            }
         }
 
         public Vector3 GetWorldPosition(int x, int y)
--- Assets/Scripts/View/PieceView.cs
+++ Assets/Scripts/View/PieceView.cs
@@ -1,9 +1,12 @@
+using System;
+using DG.Tweening;
 using UnityEngine;
 using Match3.Model;
 
 namespace Match3.View
 {
     [RequireComponent(typeof(SpriteRenderer))]
+    [RequireComponent(typeof(Collider2D))]
     public class PieceView : MonoBehaviour
     {
         private SpriteRenderer _spriteRenderer;
@@ -25,12 +28,33 @@ namespace Match3.View
             Color = color;
             Type = type;
             _spriteRenderer.sprite = sprite;
+            transform.localScale = Vector3.one;
         }
 
         public void UpdatePosition(int x, int y)
         {
             X = x;
             Y = y;
+        }
+
+        public void MoveTo(Vector3 targetWorldPos, float duration, Ease ease = Ease.OutCubic, Action onComplete = null)
+        {
+            transform.DOMove(targetWorldPos, duration).SetEase(ease).OnComplete(() => onComplete?.Invoke());
+        }
+
+        public void PlayDestroyAnimation(float duration, Action onComplete = null)
+        {
+            transform.DOScale(Vector3.zero, duration).SetEase(Ease.InBack).OnComplete(() =>
+            {
+                onComplete?.Invoke();
+                Destroy(gameObject);
+            });
+        }
+
+        public void PlaySpawnAnimation()
+        {
+            transform.localScale = Vector3.zero;
+            transform.DOScale(Vector3.one, 0.25f).SetEase(Ease.OutBack);
         }
     }
 }
